4fd6f8706b1d2f2f98ff7c4519bcf8fc76f18893
lurldgbodex <gbodisegun@gmail.com>
2022-06-02 23:14:27 +0000

    args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+
+    args_len = len(sys.argv) - 1
+    if args_len == 0:
+        print("0 arguments.")
+    elif args_len == 1:
+        print("1 arguments:")
+    else:
+        print("{} arguments:".format(args_len))
+    for args in range(1, args_len):
+        print("{}: {}".format(args, sys.argv[args]))
