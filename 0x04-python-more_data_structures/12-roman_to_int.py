733a0e9a8dc2d7e8b86450acd44629d61eac711a
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2022-06-10 14:38:19 +0100

    Update 12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,12 @@
 def roman_to_int(roman_string):
     if not roman_string or type(roman_string) != str:
         return 0
-    roman_digit = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    roman_no = 0
+    roman_d = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    roman_n = 0
     for j in range(len(roman_string)):
-        if j > 0 and roman_digit[roman_string[j]] > roman_digit[roman_string[j - 1]]:
-            roman_no += roman_digit[roman_string[j]] - 2 * \
-                        roman_digit[roman_string[j - 1]]
+        if j > 0 and roman_d[roman_string[j]] > roman_d[roman_string[j - 1]]:
+            roman_n += roman_d[roman_string[j]] - 2 * \
+                        roman_d[roman_string[j - 1]]
         else:
-            roman_no += roman_digit[roman_string[j]]
-    return roman_no
+            roman_n += roman_d[roman_string[j]]
+    return roman_n
