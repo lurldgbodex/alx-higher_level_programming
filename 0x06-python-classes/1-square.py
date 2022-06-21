e8721a595640916866c218d57a06fc00412177da
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2022-06-21 22:06:06 +0100

    update documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,8 @@ class Square:
     """Class representation of a square."""
 
     def __init__(self, size):
-        """__init__ function for initialisation of size"""
+        """__init__ function for initialisation of size
+        Args:
+            size (int): size of the new square.
+        """
         self.__size = size
