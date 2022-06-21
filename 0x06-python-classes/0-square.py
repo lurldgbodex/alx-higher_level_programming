f5488c7a6c78cf20e8fa084269333e3799b57268
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2022-06-21 21:26:21 +0100

    Update documentation

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+"""Defines a class"""
+
+
+
 class Square:
     """class representation of a square.
-
     This is an empty class representation of a square. it has neither an attribute or a method to call.
     """
     pass
