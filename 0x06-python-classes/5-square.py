da592193bc8120e3fc9505d35fb3aad05634807f
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2022-06-21 22:27:06 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 """Define a class"""
 
+
 class Square:
-    """Class representation of a square.
-    private instance of attribute size
-    instantiation with optional size
-    public instance method def area(self)
-    public instance method def my_print(self)
-    """
+    """Class representation of a square."""
 
     def __init__(self, size=0):
         """Initialisation of size
@@ -17,7 +13,7 @@ class Square:
     @property
     def size(self):
         """Retrieves and return the value of size"""
-        return self.__size
+        return (self.__size)
 
     @size.setter
     def size(self, value):
@@ -30,14 +26,14 @@ class Square:
 
     def area(self):
         """Returns the current square are"""
-        return self.__size ** 2
+        return (self.__size ** 2)
 
     def my_print(self):
         """prints in stdout the square with character #"""
-        if self.__size > 0:
+        if self.__size == 0:
             print('')
         else:
             for i in range(0, self.__size):
                 for j in range(0, self.__size):
                     print("#", end="")
-            print()
+            print('')
