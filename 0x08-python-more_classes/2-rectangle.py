b74559897a3c33bd23900d3115083dfcb18ba6e4
lurldgbodex <gbodisegun@gmail.com>
2022-06-27 19:46:20 +0000

    fix error

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -64,4 +64,4 @@ class Rectangle:
         """
         if self.__height == 0 or self.__width == 0:
             return 0
-        return 2 * (self.__height + self.__height)
+        return 2 * (self.__height + self.__width)
