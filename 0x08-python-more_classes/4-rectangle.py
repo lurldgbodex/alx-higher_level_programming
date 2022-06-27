8d5a13eba01df8f151272c2cb473a1582657eda2
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2022-06-27 21:12:13 +0100

    update file

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -27,13 +27,66 @@ class Rectangle:
                 str_ret += '#'
             str_ret += '\n'
         return str_ret[:-1]
-    
+
     def __repr__(self):
         """
-        return a string representation of a rectangle instance
-        that is able to recreate a new instance by using evel() 
+        :returns a string representation of a rectangle instance
+        that is able to recreate a new instance by using eval()
         """
         return "Rectangle({}, {})".format(self.__width, self.__height)
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        sets the value of the width property
+        :param value: parameter that takes the width argument
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        gets the value of height
+        :return: the value of height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        sets the value of height
+        :param value: the value to set
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """
+        calculates area of rectangle
+        :return: The area of the rectangle calculate
+        """
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """
+        calculates the perimeter of a rectangle
+        :return: the calculated perimeter
+        """
+        if self.__height == 0 or self.__width == 0:
+            return 0
+        return 2 * (self.__height + self.__width)
+
 
     @property
     def width(self):
