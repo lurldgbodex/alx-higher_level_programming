e24d8ae79ed6326f2a13371868c1c07e0f2593c0
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2022-06-27 20:35:23 +0100

    update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -10,7 +10,7 @@ class Rectangle:
     class implementation of a Rectangle
     """
 
-    def __init__(self, width, height):
+    def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
 
@@ -24,7 +24,7 @@ class Rectangle:
         sets the value of the width property
         :param value: parameter that takes the width argument
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -44,7 +44,7 @@ class Rectangle:
         sets the value of height
         :param value: the value to set
         """
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
