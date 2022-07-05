7c50b21a837f98d5d041b03ff3ac06635cf5b4a6
lurldgbodex <gbodisegun@gmail.com>
2022-07-05 12:04:53 +0000

    add file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Reads from a file
+"""
+
+
+def number_of_lines(filename=""):
+    """
+    counts the number 0f lines in filename.
+    :param filename: name of the file
+    :return: number of lines
+    """
+
+    count = 0
+
+    with open(filename) as f:
+        text = f.readlines()
+        for _ in text:
+            count += 1
+
+    return count
