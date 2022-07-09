88c2a892355b4e5fb90c393154e00ae4db86cc6f
lurldgbodex <gbodisegun@gmail.com>
2022-07-09 09:05:43 +0000

    refactor code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
 """
-Reads from a file
+writes to a file
 """
 
 
-def number_of_lines(filename=""):
+def write_file(filename=""):
     """
-    counts the number 0f lines in filename.
+    writes a string to a textfile.
     :param filename: name of the file
-    :return: number of lines
+    :return: number of characters written
     """
 
-    count = 0
-
-    with open(filename) as f:
-        text = f.readlines()
-        for word in text:
-            count += 1
-
-    return count
+    with open(filename, "w", encoding="utf-8") as f:
+        text = f.write()
