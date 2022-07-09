1de9aaea1b4fda190e2b17fd528769c691fea574
lurldgbodex <gbodisegun@gmail.com>
2022-07-09 09:41:23 +0000

    add file

---
+++ 0x0B-python-input_output/4-from_json_sting.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+returns an object represented by Json string
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    :param my_str: object to return from JSON representation
+    :return: from JSON representation to a string object
+    """
+    return json.load(my_str)
