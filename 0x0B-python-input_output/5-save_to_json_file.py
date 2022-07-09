620ecb1ae1e4819699c232eda68cce65e23cb51e
lurldgbodex <gbodisegun@gmail.com>
2022-07-09 09:57:46 +0000

    update file

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,4 +10,5 @@ def save_to_json_file(my_obj, filename):
     :param my_obj: object to write to file in JSON representation
     :param filename: name of the file
     """
-    json.dump(my_obj, filename)
+    with open(filename, "w") as f:
+        json.dump(my_obj, f)
