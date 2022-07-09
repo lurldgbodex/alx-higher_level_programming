f06322ef157545115ad797dd2be50d474198c3c2
lurldgbodex <gbodisegun@gmail.com>
2022-07-09 09:11:46 +0000

    update code

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,12 +4,13 @@ writes to a file
 """
 
 
-def write_file(filename=""):
+def write_file(filename="", text=""):
     """
     writes a string to a textfile.
     :param filename: name of the file
+    :param text: contains the string to write to file
     :return: number of characters written
     """
 
     with open(filename, "w", encoding="utf-8") as f:
-        text = f.write()
+        return f.write(text)
