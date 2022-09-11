4764f07a05549019ef8abea72447e653358235bf
Lurldgbodex <gbodisegun@gmail.com>
2022-09-11 17:59:47 +0000

    updating code in task zero for checker "

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -10,7 +10,6 @@ if __name__ == '__main__':
                          user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
 
     cur = db.cursor()
-    cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
+    num_rows = cur.execute("SELECT * FROM states ORDER BY states.id ASC;")
+    for row in range(num_rows):
+        print(cur.fetchone())
