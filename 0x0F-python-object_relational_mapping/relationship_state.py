67163518d63f6f9501781ab764a21b695a2329e3
Lurldgbodex <professioanleditor6@gmail.com>
2022-09-16 13:26:20 -0700

    update code of files for task 15 city relationship

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -3,13 +3,17 @@
 """
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from relationshp_city import City
 
 Base = declarative_base()
 
 
 class State(Base):
-    """State model"""
+    """ Class State model"""
+
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True,
-                nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+
+    cities = relationship("City", backref="state", cascade="all, delete")
