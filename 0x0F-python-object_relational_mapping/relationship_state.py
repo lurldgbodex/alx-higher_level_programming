f683c14372c93c3f3a8f4fd49796152dacb44bdf
Lurldgbodex <professioanleditor6@gmail.com>
2022-09-16 13:30:10 -0700

    refacter code of files relationship_cities and _states.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-"""State model with SQLAlchemy
-"""
-from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
-from relationshp_city import City
+"""Definition of the State class with relationship to City"""
 
-Base = declarative_base()
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
+from relationship_city import City, Base
 
 
 class State(Base):
-    """ Class State model"""
+    """Class State"""
 
     __tablename__ = 'states'
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, autoincrement=True,
+                primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-
-    cities = relationship("City", backref="state", cascade="all, delete")
+    cities = relationship('City', backref='state',
+                          cascade='all, delete-orphan')
